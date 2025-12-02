1e57c84fceadfaa568df8252e599423fcd28655f
Ingeniería Trejos <fatreber85@gmail.com>
2025-12-02 16:07:34 -0500

    Habilitar acceso publico a API

--- polleria_digital/productos/views.py
+++ polleria_digital/productos/views.py
@@ -1,7 +1,7 @@
-# productos/views.py
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny # <-- IMPORTACIÓN CRÍTICA
+# Importación crítica para permitir acceso sin login
+from rest_framework.permissions import AllowAny 
 from .models import Producto, Orden
 from .serializers import ProductoSerializer, OrdenSerializer
 
@@ -9,19 +9,22 @@ class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
     # --- CORRECCIÓN CRÍTICA ---
-    # Permitimos que CUALQUIERA (AllowAny) acceda a esta API.
-    permission_classes = [AllowAny] 
+    # Permitimos que CUALQUIERA vea el menú.
+    permission_classes = [AllowAny]
     # ---------------------------
 
 class OrdenViewSet(viewsets.ModelViewSet):
-    # NOTA IMPORTANTE: Esta API (Ordenes) ahora SÍ requiere que el usuario esté autenticado 
-    # por la configuración general en settings.py. Esto es correcto, ya que nadie debería 
-    # poder crear un pedido sin una cuenta. 
     queryset = Orden.objects.all().order_by('-fecha_pedido')
     serializer_class = OrdenSerializer
+    
+    # --- AJUSTE TEMPORAL ---
+    # Permitimos crear órdenes sin login para que el checkout funcione 
+    # mientras construimos el módulo de Autenticación en el frontend.
+    permission_classes = [AllowAny]
+    # -----------------------
 
     def create(self, request, *args, **kwargs):
-        # Tu lógica avanzada de cálculo y guardado iría aquí
+        # Tu lógica avanzada de cálculo y guardado
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
